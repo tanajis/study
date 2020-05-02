90ccf0cae802a3466569f374cf14f66468aa455d
Tanaji Sutar <tanajisutar31@gmail.com>
2020-05-02 13:54:41 +0530

    Update starthadoop.sh

--- BigData/Apache Hadoop/scripts/starthadoop.sh
+++ BigData/Apache Hadoop/scripts/starthadoop.sh
@@ -1,18 +1,24 @@
-#@Auther : Tanaji Sutar
-#This file is created to start the hadoop cluster
-#Install hadoop from https://www.michael-noll.com/tutorials/running-hadoop-on-ubuntu-linux-single-node-cluster/
-# To run this file use ./starthadoop.sh
 
+##########################################################################################
+# Auther : Tanaji Sutar
+# Desc   :This file is created to start the hadoop cluster and spark.
+#         Install hadoop from https://www.michael-noll.com/tutorials/running-hadoop-on-ubuntu-linux-single-node-cluster/
+#         To run this file use ./starthadoop.sh
+# Note   : u must set HADOOP_HOME and SPARK_HOME before running this script.
+# Date   : 02-05-2020
+##########################################################################################
 
-cd /home/tms/tmsCodeGround/myInstallDir/hadoop-2.7.3/sbin
+cd $HADOOP_HOME/sbin
 ./start-yarn.sh
 ./start-dfs.sh
 
-#You can access the hadoop from web browser as well
-#HDFS : http://localhost:50070/
-#RM UI: http://localhost:8088/
+# You can access the hadoop from web browser as well
+# HDFS : http://localhost:50070/
+# RM UI: http://localhost:8088/
 
-
-#Start Spark
-cd /home/tms/tmsCodeGround/myInstallDir/spark-2.3.2-bin-hadoop2.7/sbin
+# Start Spark
+cd $SPARK_HOME/sbin
 ./start-all.sh
+
+# Show all the deamon
+jps
